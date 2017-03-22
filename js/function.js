9e725c9132256e69971ade22ecdd289e6cb658ba
supermanc88 <supermanc88@gmail.com>
2017-03-22 17:42:46 +0800

    添加玫瑰线方程

--- js/function.js
+++ js/function.js
@@ -4,7 +4,7 @@
  * 
  */
 
-r = 8;
+r = 20;
 randianDecrement = 0; //弧度增量
 i = 0;
 intervalId = 0;
@@ -16,7 +16,7 @@ function drawHeart(){
 	ctx.lineWidth = 1;	//设置线的宽度
 
 	//randian = 0; //初始弧度为0  取值范围为[0,2π];
-	randianDecrement = Math.PI / 360 * 2;
+	randianDecrement = Math.PI / 180 * 2;
 
 	ctx.moveTo(getX(randian), getY(randian)); //移动画笔到初始位置
 	// alert(getX(randian));
@@ -28,18 +28,70 @@ function drawHeart(){
 function printHeart(){
 	randian += randianDecrement;
 	ctx.lineTo(getX(randian), getY(randian));
+
+	garden.createRandomBloom(getX(randian), getY(randian));
+
 	i++
 	ctx.stroke();//划线
 
-	if(i > 360){
+	if(i > 180){
 		clearInterval(intervalId);
 	}
 }
 
 function getX(t) {//由弧度得到 X 坐标  
-    return 200 + r * (16 * Math.pow(Math.sin(t), 3));  
+    return WINDOW_WIDTH / 2 + r * (16 * Math.pow(Math.sin(t), 3));  
 }  
 
 function getY(t) {//由弧度得到 Y 坐标  
-    return 200 - r * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));  
+    return WINDOW_HEIGHT / 2 - r * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));  
+}
+
+
+
+//玫瑰花线数学公式
+//x=a* sin(nθ)* cos(θ), y=a*sin(nθ)* sin(θ)
+
+var a = 0.5;
+var n = 0.2; 
+var angle = 0;
+var angleDecrement = 0; //角度增量
+var zhouqi = Math.PI*2;
+
+var roseIntervalId = 0;
+var dd = 0;
+
+function drawRose(){
+	ctx.strokeStyle = "red";   //设置笔触的颜色
+	ctx.lineWidth = 1;	//设置线的宽度
+
+	angleDecrement = zhouqi / 360;
+
+	var point = getRosePoint(angle);
+
+	ctx.moveTo(point[0], point[1]);
+
+	roseIntervalId = setInterval("printRose()", intervalTime);
+}
+
+function printRose(){
+
+	angle += angleDecrement;
+
+	var point = getRosePoint(angle);
+	ctx.lineTo(point[0], point[1]);
+	ctx.stroke();
+
+	dd ++;
+
+	if(dd > 360){
+		clearInterval(roseIntervalId);
+	}
+
+}
+
+function getRosePoint(t){
+	var x = WINDOW_WIDTH / 2 + 10 * a * Math.sin(n * t) * Math.cos(t);
+	var y = WINDOW_HEIGHT / 2 + 10 * a * Math.sin(n * t) * Math.sin(t);
+	return new Array(x, y); 
 }
